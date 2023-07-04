ac8a4d038d9021ce384a0ebacf8029016b2f76fa
phrstyle <joaovitorpinheiroborges@gmail.com>
2023-07-04 09:11:32 -0400

    04/07

--- instagram.py
+++ instagram.py
@@ -1,7 +1,7 @@
 from instaloader import Instaloader, Profile
 from datetime import datetime, timedelta
-import correcao_pasta_instagram
 import os
+from os import walk
 import pathlib
 
 #Função responsável por executar o download das publicações do Instagram Oficial do IFMT
@@ -9,7 +9,7 @@ def download_instagram():
     #Realiza o download das publicações dos últimos 5 dias
     data_inicio = datetime.today() - timedelta(days=6)
     L=Instaloader()
-    L.login("testeifmt", "teste123")
+    L.login("testeifmt", "teste@123")
     PROFILE = 'ifmtcuiabaoficial'
     profile = Profile.from_username(L.context, PROFILE)
     post_sorted = sorted(profile.get_posts(),key=lambda post: post.likes, reverse=True)
@@ -18,5 +18,30 @@ def download_instagram():
         if post.date >= data_inicio or post.is_pinned:
             L.download_post(post, pathlib.Path('static/ifmtcuiabaoficial'))
     #Chama a função de correção da pasta com os arquivos do Instagram
-    correcao_pasta_instagram.correcao_pasta()
+    correcao_pasta()
 
+def correcao_pasta():
+    files = []
+    path = 'static/ifmtcuiabaoficial'
+    for (dirpath, dirnames, filenames) in walk(path):
+        files.extend(filenames)
+        break
+    tamanho = len(files)
+    for i in range(tamanho):
+        #Se existir um arquivo .mp4, ele busca o arquivo .jpg com o mesmo nome
+        if files[i].endswith('.mp4'):
+            aux1 = os.path.splitext(files[i])[0]
+            for j in range(tamanho):
+                if files[j].endswith('.jpg'):
+                    aux2 = os.path.splitext(files[j])[0]
+                    #Se existir um arquivo .jpg (frame inicial do .mp4), ele é excluído
+                    if aux2 == aux1:
+                        lix = os.path.join(path, files[j])
+                        if os.path.isfile(files[j]) == False:
+                            os.remove(lix)
+    #Busca todos os arquivos da pasta ifmtcuiabaoficial
+    listainsta = os.listdir("static/ifmtcuiabaoficial")
+    for l in listainsta:
+        #remove todos os arquivos que nao sao videos/imagens
+        if ".mp4" not in l and ".jpg" not in l and ".jpeg" not in l and ".png" not in l:
+            os.remove("static/ifmtcuiabaoficial/" + l)
--- noticias.py
+++ noticias.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-import correcao_arquivo_csv
+import os
 
 #Função responsável por executar o Web Scraping da página de notícias do IFMT
 def scraping_noticias():
@@ -20,4 +20,28 @@ def scraping_noticias():
     #Cria um arquivo .csv para armazenar os títulos
     news.to_csv('noticias.csv', index=False)
     #Chama a função de correção das falhas no arquivo .csv
-    correcao_arquivo_csv.correcao_csv()
+    correcao_csv()
+
+#Função que corrige e cria novo arquivo .csv com as notícias
+def correcao_csv():
+    #Abre o arquivo .csv atual apenas para leitura
+    arquivo_noticias = 'noticias.csv'
+    file = open(arquivo_noticias, 'r')
+    linhas = file.readlines()
+    qtd = 6
+    file.close()
+    #Cria novo arquivo para gravação
+    new_file = open('noticias-ifmt.csv', 'w')
+    #A intenção é manter apenas as 5 primeiras notícias (as principais da página inicial)
+    for i in range(qtd):
+        if i==0:
+            #Ele pula a primeira linha, pois o arquivo original inicia com "0"
+            new_file.writelines('')
+        else:
+            #Ele escreve no novo arquivo as linhas restantes
+            novas_linhas = linhas[i]+' | '
+            new_file.writelines(novas_linhas)
+    new_file.close()
+    #Exclui o arquivo .csv antigo, se ele existir
+    if os.path.exists(arquivo_noticias):
+        os.remove(arquivo_noticias)
